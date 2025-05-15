c4e6e4c4d6721991ace104097e5d9e5400506982
ccorbin <calcorbin@gmail.com>
2025-05-15 15:35:28 -0500

    Add more style to carousel container

--- src/app/product-dashboard/page.js
+++ src/app/product-dashboard/page.js
@@ -4,7 +4,13 @@ import ProductCarousel from '../components/ProductCarousel/ProductCarousel.jsx';
 export default function ProductDashboard() {
   return (
     <div>
-      <h1>Product Dashboard</h1>
+      <h1
+        style={{
+          padding: '1rem',
+        }}
+      >
+        Product Dashboard
+      </h1>
       <ProductCarousel
         title="Most Reviewed"
         queryParams={{ limit: 10, mostReviewed: true }}
